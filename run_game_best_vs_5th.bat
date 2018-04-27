def69050a072d0f237f6417bfb7e806de0eb1afb
zouh520sdo <zouh520sdo@gmail.com>
2018-04-26 20:52:03 -0400

    modify python call

--- run_game_best_vs_5th.bat
+++ run_game_best_vs_5th.bat
@@ -1,3 +1,3 @@
 rem javac MyBot.java
 rem javac HuangBot.java
-halite -d "240 160" "java HuangBot 1th_best" "python mellendo/Halite2/MyBot.py"
+halite -d "240 160" "java HuangBot best" "py mellendo/Halite2/MyBot.py"
